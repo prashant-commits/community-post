fdf7eb894f2d8e6440224295bc7fd111ab361a5e
Gsangi <sanskar6053@gmail.com>
2023-08-24 02:48:24 +0530

    feat: add logout toast

--- src/pages/Logout/index.tsx
+++ src/pages/Logout/index.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useApp } from "../../contexts/App";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const LogoutPage: React.FC = () => {
-    const {logoutUser} = useApp()
+    const {user, logoutUser} = useApp()
     const navigate = useNavigate()
 
     useEffect(() => {
         logoutUser()
         navigate("/login")
+        toast(`${user?.username} is logged out`)
     }, [])
 
     return null;
